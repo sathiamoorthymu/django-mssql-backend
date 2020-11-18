c827fbf0e7479da73f77c83b689fc0e9cb511cdc
Sathia <sathiamoorthy.mu@tigeranalytics.com>
2020-11-18 16:34:02 +0530

    New version created

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ CLASSIFIERS = [
 
 setup(
     name='django-mssql-azure-backend',
-    version='1.0.0',
+    version='1.0.1',
     description='Django backend for Microsoft SQL Server',
     long_description=open('README.rst').read(),
     author='Sathiamoorthy M',
